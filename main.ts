d546c133df202002915e2c1442df1e5a47071e0c
Calvin Wilkinson <kinsondigital@gmail.com>
2023-09-01 18:18:09 +0100

    test

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-Deno.serve((req) => new Response("Hello Kinson!"));
+
+Deno.serve((req) => {
+
+    // Get the query parameters from the incoming deno request
+    const params = new URLSearchParams(req.url.split("?")[1]);
+
+    const reqParams: string[] = [];
+
+    params.forEach((value, key) => {
+        reqParams.push(`${key}: ${value}`);
+    });
+
+    const result = reqParams.join("\n");
+
+    return new Response(result);
+});
