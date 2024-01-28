24cc02b6d469e7d4870278d07e6a7923e1b99b97
Calvin Wilkinson <kinsondigital@gmail.com>
2024-01-28 14:26:27 +0000

    test

--- main.ts
+++ main.ts
@@ -2,14 +2,19 @@ import { TagClient } from "./deps.ts";
 
 Deno.serve(async (req) => {
 
-    const token = Deno.args[0];
+    const token = Deno.env.get("GITHUB_TOKEN") ?? "not-set";
+    const haveToken = token.startsWith("ghp_");
+
+    const reqInfo = `URL: ${req.url}` +
+        `\nMethod: ${req.method}` +
+        `\nHave Token: ${haveToken}`;
+
+
     const tagClient: TagClient = new TagClient("KinsonDigital", "Velaptor", token);
-    
     const tags = await tagClient.getAllTags();
-
     const tagVersions = tags.map((tag) => tag.name);
-
-    const body: BodyInit = JSON.stringify(tagVersions);
+    // const body: BodyInit = JSON.stringify(tagVersions);
+    const body: BodyInit = reqInfo;
 
     return new Response(body);
 });
